4ff9f293a8c59965b6301804868421046446e76e
gitauto <massimo.conti@inwind.it>
2025-02-24 23:12:57 +0100

    progetto maui iniziato

---
+++ MauiApp1/Helpers/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace MauiApp1.Helpers;
+
+public static class Utils
+{
+    public static string GetAppVersion()
+    {
+        Version? version = Assembly.GetExecutingAssembly().GetName().Version;
+        return $"ver. {version?.Major}.{version?.Minor}.{version?.Build}.{version?.Revision}";
+    }
+}
--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,12 +1,32 @@
-﻿namespace MauiApp1;
+﻿using DumpLibrary;
+using MauiApp1.Helpers;
+using System.Globalization;
+using System.Net.Http;
+
+namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
+    private const string _HTMLPageTitle = "Dump Playground";
+    private readonly string _isoCode = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+
     public MainPage()
     {
         InitializeComponent();
 
-        webView.Source = new System.Uri("https://www.google.com");
+        this.Title = $".NET MAUI - {_HTMLPageTitle} ver. {Utils.GetAppVersion()}";
+        DumpExtensions.AppendRawHTML = AppendRawHTML;
+
+        var htmlSource = new HtmlWebViewSource { Html = DumpExtensions.GetHtmlPageTemplate("Dump Playground", _isoCode, false) };
+
+        // Assegna la sorgente al WebView
+        webView.Source = htmlSource;
+    }
+
+    public async void AppendRawHTML(string html)
+    {
+        var script = $@"appendRawHTML(`{html}`)";
+        await webView.EvaluateJavaScriptAsync(script);
     }
 }
 
